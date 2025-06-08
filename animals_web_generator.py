6deac7e0c6eebc9a4d41b17e0991b65946eb6582
nadim00100 <nadim.almasri0@gmail.com>
2025-06-08 17:50:49 +0200

    Milestone 1 fetches the information from the API

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,17 +1,26 @@
-import json
+import requests
 
 
-def load_data(file_path):
-    """Load data from a JSON file."""
-    with open(file_path, "r") as handle:
-        return json.load(handle)
+def fetch_animal_data(name):
+    """Fetch animal data from the API."""
+    api_url = f'https://api.api-ninjas.com/v1/animals?name={name}'
+    headers = {'X-Api-Key': 'oesLxIwq+McVd7lwocWkxw==TsdRlaC9UShkQgsa'}
+    response = requests.get(api_url, headers=headers)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Failed to fetch data. Status code: {response.status_code}")
+        return []
 
 
 def serialize_animal(animal_obj):
     """Generate HTML list item for a single animal."""
     name = animal_obj.get('name', 'Unknown')
     diet = animal_obj.get('characteristics', {}).get('diet', 'Unknown')
-    locations = ', '.join(animal_obj.get('locations', []))
+    locations = animal_obj.get('locations', [])
+    location = locations[0] if locations else 'Unknown'
+   #print(name,location)
     animal_type = animal_obj.get('characteristics', {}).get('type', 'Unknown')
 
     return f"""
@@ -20,7 +29,7 @@ def serialize_animal(animal_obj):
             <div class="card__content">
                 <h2 class="card__title">{name}</h2>
                 <p><strong>Diet:</strong> {diet}</p>
-                <p><strong>Locations:</strong> {locations}</p>
+                <p><strong>Location:</strong> {location}</p>
                 <p><strong>Type:</strong> {animal_type}</p>
             </div>
         </div>
@@ -28,9 +37,10 @@ def serialize_animal(animal_obj):
     """
 
 
+
 def main():
-    # Load JSON data
-    animals_data = load_data('animals_data.json')
+    # Fetch data from the API for "Fox"
+    animals_data = fetch_animal_data("fox")
 
     # Load HTML template
     with open('animals_template.html', 'r') as fileobj:
@@ -39,8 +49,10 @@ def main():
     # Build the output string with animal info
     output = ''.join(serialize_animal(animal) for animal in animals_data)
 
+
     # Replace the placeholder with the actual animal info
     html_content = content.replace("__REPLACE_ANIMALS_INFO__", output)
+
 
     # Write to a new HTML file
     with open('animals.html', 'w') as fileobj:
