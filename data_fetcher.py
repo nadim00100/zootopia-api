f4392b519839f182f8f44ebec50830c174a635cc
nadim00100 <nadim.almasri0@gmail.com>
2025-06-08 18:56:21 +0200

    Create an .env file and get the api_key from it

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,4 +1,10 @@
 import requests
+import os
+from dotenv import load_dotenv
+
+# Load .env once when the module is imported
+load_dotenv()
+API_KEY = os.getenv('API_KEY')
 
 def fetch_data(animal_name):
     """
@@ -17,8 +23,12 @@ def fetch_data(animal_name):
       }
     }
     """
+    if not API_KEY:
+        print("Error: API_KEY not found in environment variables.")
+        return []
+
     api_url = f'https://api.api-ninjas.com/v1/animals?name={animal_name}'
-    headers = {'X-Api-Key': 'oesLxIwq+McVd7lwocWkxw==TsdRlaC9UShkQgsa'}
+    headers = {'X-Api-Key': API_KEY}
 
     response = requests.get(api_url, headers=headers)
 
